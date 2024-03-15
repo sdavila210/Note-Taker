bedc1a29d1bd9827aa1d20ff8fab231f985bb3db
sarahd <sarahjdavila210@gmail.com>
2024-03-14 22:38:49 -0700

    added comments to code for server.js

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+// Importing required modules/packages & setting up the port for the server
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
@@ -13,15 +14,17 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
-// HTML Routes for notes.html and index.html
+// HTML Routes which handles GET request for notes.html
 app.get('/notes', (req, res) =>
     res.sendFile(path.join(__dirname, 'public/notes.html'))
 );
 
+// HTML routes which handles GET request for index.html
 app.get('/', (req, res) =>
     res.sendFile(path.join(__dirname, 'public/index.html'))
 );
 
+// Handles GET request for retrieving notes
 app.get('/api/notes', (req, res) => {
     fs.readFile('db/db.json', 'utf8', (err, result) => {
         const data = JSON.parse(result)
@@ -30,6 +33,7 @@ app.get('/api/notes', (req, res) => {
 }
 )
 
+// Handles POST requests for adding new notes. A new note object is created with data from the request body and unique ID
 app.post('/api/notes', (req, res) => {
     const newNote = {
         title: req.body.title,
@@ -37,6 +41,8 @@ app.post('/api/notes', (req, res) => {
         id: uuidv4()
     }
     console.log(newNote)
+
+    // Read data from database file, add new note to array, write data back to database file, and send new note as JSON response
     fs.readFile('db/db.json', 'utf8', (err, result) => {
         const data = JSON.parse(result)
         data.push(newNote)
